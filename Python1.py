424f98ec5903ea08a5dbc21ead23c434f4052965
aclementepa <aclementepa@gmail.com>
2022-11-02 13:16:37 -0400

    Update Python1.py

--- Python1.py
+++ Python1.py
@@ -23,3 +23,18 @@ for word in list2:
 d = {'a': 1, 'c': 3, 'e': 5}
 asdf = [v for k,v in d.items()] # [1, 5, 3]
 fdsa = [k for k,v in d.items()] # ['a', 'e', 'c']
+
+str1 = "ABC"
+str2 = "123"
+yak1 = {str1[i]: str2[i] for i in range(0,len(str2))} # {'A': '1', 'B': '2', 'C': '3'}
+
+num_list = [1,2,3,4]
+# print({num:("even" if num % 2 == 0 else "odd") for num in num_list})
+yak2 = {num:("even" if num % 2 == 0 else "odd") for num in num_list}
+
+# for key,val in yak2.items():
+#     print(val)
+
+x = (1,2,3)
+if(3 in x ):
+    print(x)
